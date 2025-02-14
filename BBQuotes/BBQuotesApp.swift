3f7b711b3e86d6d8bd6a8ca7a904ec9091a1a897
alcode111 <al.code.111@gmail.com>
2025-02-14 19:11:25 +0100

    Import XCode project

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Ismaïl on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
