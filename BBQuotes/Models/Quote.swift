f6f3786a99a35317f539ec6a4863d371f0662a8a
alcode111 <al.code.111@gmail.com>
2025-02-14 20:07:43 +0100

    Create main 3 models: Char, Quote, Death

---
+++ BBQuotes/Models/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  BBQuotes
+//
+//  Created by Ismaïl on 14/02/2025.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}
