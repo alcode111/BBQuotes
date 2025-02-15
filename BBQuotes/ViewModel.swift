8797034a598a11c8bf339f7f3baa692479d38d17
alcode111 <al.code.111@gmail.com>
2025-02-15 10:28:04 +0100

    Start creating ViewModel

---
+++ BBQuotes/ViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ViewModel.swift
+//  BBQuotes
+//
+//  Created by Ismaïl on 15/02/2025.
+//
+
+import Foundation
+
+@Observable
+@MainActor
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed(error: Error)
+    }
+    
+    private(set) var status: FetchStatus = .notStarted
+    private let fetcher = FetchService()
+    
+    var quote: Quote
+    var character: Char
+    
+    init() {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let quoteData = try! Data(contentsOf: Bundle.main.url(forResource: "samplequote", withExtension: "json")!)
+        quote = try! decoder.decode(Quote.self, from: quoteData)
+        
+        let characterData = try! Data(contentsOf: Bundle.main.url(forResource: "samplecharacter", withExtension: "json")!)
+        character = try! decoder.decode(Char.self, from: characterData)
+    }
+}
